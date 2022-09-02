40197900f86411366258608c1d69a6f1b0ae5ff0
ConstantinGSt <stepanyuk.kon@yandex.ru>
2022-09-02 16:27:25 +0700

    add maven project

---
+++ maxima-java-01/src/main/java/org/example/App.java
@@ -0,0 +1,17 @@
+package org.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main(String[] args) {
+        int firstVar = 10;
+        int secondVar = 15;
+        //first method
+       /* secondVar = secondVar - 5;
+        firstVar = firstVar + 5; */
+        int tempVar = firstVar + secondVar;
+        secondVar = tempVar -s
+    }
+}
