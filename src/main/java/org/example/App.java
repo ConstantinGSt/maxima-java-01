516fc7f87a2556962945368f6c5ee85edd2e4de9
ConstantinGSt <stepanyuk.kon@yandex.ru>
2022-09-06 11:41:03 +0700

    add recursion fibo

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -5,19 +5,30 @@ package org.example;
  *
  */
 public class App {
-    public static void main(String[] args) {
-        int firstVar = 10;
-        int secondVar = 15;
-        System.out.println("firstVar = " + firstVar);
-        System.out.println("secondVar = " + secondVar);
-        //first method
-       /* secondVar = secondVar - 5;
-        firstVar = firstVar + 5; */
-        // method two
-        int tempVar = secondVar;
-        secondVar = firstVar;
-        firstVar = tempVar;
-        System.out.println("firstVar = " + firstVar);
-        System.out.println("secondVar = " + secondVar);
-    }
+	public static void main(String[] args) {
+
+		int firstVar = 10;
+		int secondVar = 15;
+		System.out.println("firstVar = " + firstVar);
+		System.out.println("secondVar = " + secondVar);
+
+		int tempVar = secondVar;
+		secondVar = firstVar;
+		firstVar = tempVar;
+		System.out.println("firstVar = " + firstVar);
+		System.out.println("secondVar = " + secondVar);
+
+		// fibonazzi recursive
+		System.out.println(fibonazzi((byte) 50));
+	}
+
+	public static long fibonazzi(byte N) {
+		if (N == 0) {
+			return 0;
+		} else if (N == 1) {
+			return 1;
+		} else {
+			return fibonazzi((byte) (N - 1)) + fibonazzi((byte) (N - 2));
+		}
+	}
 }
